90b5c1e334bebd59aef3116596c59843d9d04b9e
overmyerD <dalton@raptorpowersystems.com>
2024-04-30 14:57:23 -0500

    add uv_ntp_time_to_str and printing all various timestamps

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ int main(int argc, char** argv) {
     return r;
   }
 
-  // poll NTP server "ntp1.stratum1.ru" every 5 seconds
-  r = uv_ntp_start(&ntp, "time.euro.apple.com", poll_cb, 5 * 1000);
+  // poll NTP server "time-c-b.nist.gov" every 5 seconds
+  r = uv_ntp_start(&ntp, "time-c-b.nist.gov", poll_cb, 5 * 1000);
   if (r < 0) {
     fprintf(stderr, "ERROR: %s\n", uv_strerror(r));
     return r;
@@ -39,10 +39,23 @@ struct timeval uv_ntp_to_timeval(uint32_t second, uint32_t fraction) {
   return tv;
 }
 
-void uv_ntp_ref_id_to_str(uint32_t ref_id, char* str, size_t str_len) {
-  if (str_len < 5) return;
+int uv_ntp_ref_id_to_str(uint32_t ref_id, char* str, size_t str_len) {
+  if (str_len < 5) return -1;
   memcpy(str, &ref_id, 4);
   str[4] = '\0';
+  return 1;
+}
+
+size_t uv_ntp_time_to_str(uint32_t second, uint32_t fraction, char* str, size_t str_len) {
+  struct timeval tv = uv_ntp_to_timeval(second, fraction);
+
+  int nwrite1 = strftime(str, str_len, "%Y/%m/%d %X", localtime(&tv.tv_sec));
+  if (nwrite1 <= 0) return -1;
+
+  int nwrite2 = snprintf(str + nwrite1, str_len - nwrite1, ".%06ld", tv.tv_usec);
+  if (nwrite2 <= 0) return -1;
+
+  return nwrite1 + nwrite2;
 }
 
 void poll_cb(uv_ntp_t* ntp, ntp_packet_t* packet, int status) {
@@ -51,12 +64,27 @@ void poll_cb(uv_ntp_t* ntp, ntp_packet_t* packet, int status) {
     return;
   }
 
-  struct timeval tv = uv_ntp_to_timeval(packet->transmit_timestamp_second, packet->transmit_timestamp_fraction);
-  char buffer[32];
-  strftime(buffer, 32, "%Y/%m/%d %X", localtime(&tv.tv_sec));
-
   char ref_id_str[5];
-  uv_ntp_ref_id_to_str(packet->reference_id, ref_id_str, 5);
+  char ref_buffer[32];
+  char origin_buffer[32];
+  char rx_buffer[32];
+  char tx_buffer[32];
+
+  if (uv_ntp_ref_id_to_str(packet->reference_id, ref_id_str, 5) < 0) {
+    return;
+  }
+  if (uv_ntp_time_to_str(packet->reference_timestamp_second, packet->reference_timestamp_fraction, ref_buffer, 32) <= 0) {
+    return;
+  }
+  if (uv_ntp_time_to_str(packet->origin_timestamp_second, packet->origin_timestamp_fraction, origin_buffer, 32) <= 0) {
+    return;
+  }
+  if (uv_ntp_time_to_str(packet->receive_timestamp_second, packet->receive_timestamp_fraction, rx_buffer, 32) <= 0) {
+    return;
+  }
+  if (uv_ntp_time_to_str(packet->transmit_timestamp_second, packet->transmit_timestamp_fraction, tx_buffer, 32) <= 0) {
+    return;
+  }
 
   printf("li:              %d\n", UV_NTP_LI(packet->li_vn_mode));
   printf("vn:              %d\n", UV_NTP_VN(packet->li_vn_mode));
@@ -66,7 +94,9 @@ void poll_cb(uv_ntp_t* ntp, ntp_packet_t* packet, int status) {
   printf("precision:       %d\n", packet->precision);
   printf("root delay:      %d\n", packet->root_delay);
   printf("root dispersion: %d\n", packet->root_dispersion);
-
   printf("reference ID:    %s\n", ref_id_str);
-  printf("TX Time:         %s\n", buffer);
+  printf("Reference Time:  %s\n", ref_buffer);
+  printf("Origin Time:     %s\n", origin_buffer);
+  printf("RX Time:         %s\n", rx_buffer);
+  printf("TX Time:         %s\n\n", tx_buffer);
 }
